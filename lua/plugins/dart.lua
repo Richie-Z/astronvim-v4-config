6667df35a7c66550b8e48a0cfd2208c09d54592f
Richie-Z <zakariarichie@yahoo.com>
2024-04-17 08:55:30 +0700

    feat(plugins): add dart ts

--- lua/plugins/dart.lua
+++ lua/plugins/dart.lua
@@ -11,6 +11,15 @@ return {
     },
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dart" })
+      end
+    end,
+  },
+  {
     "akinsho/flutter-tools.nvim",
     ft = "dart",
     opts = function(_, opts)
