c9f91f6ef4c6dfcc41a0babb4075e09c5e5c436c
Richie-Z <zakariarichie@yahoo.com>
2024-05-15 09:17:29 +0700

    feat(php): add conform for formatter and adjust

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -4,7 +4,8 @@ return {
     optional = true,
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "php", "blade" })
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "php", "blade", "phpdoc" })
       end
     end,
   },
@@ -12,7 +13,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "intelephense" })
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "phpactor", "intelephense" })
     end,
   },
   {
@@ -38,5 +40,15 @@ return {
         { "intelephense", "php-debug-adapter", "blade-formatter" }
       )
     end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "intelephense" },
+        blade = { "blade-formatter" },
+      },
+    },
   },
 }
