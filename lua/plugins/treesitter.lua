72545d375300c4fdea53b1901d28fc2d7597bad8
Richie-Z <zakariarichie@yahoo.com>
2024-05-22 16:15:38 +0700

    feat: tree sitter add git

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,5 +4,12 @@ return {
   opts = function(_, opts)
     -- disable the auto install duhhh
     opts.auto_install = false
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+    })
   end,
 }
