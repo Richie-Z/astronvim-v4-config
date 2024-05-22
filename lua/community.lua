67846c0de07a714618e1b6208be4bd31c11986bd
Richie-Z <zakariarichie@yahoo.com>
2024-05-22 15:57:38 +0700

    refactor: clean up community

--- lua/community.lua
+++ lua/community.lua
@@ -3,21 +3,21 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- UI
   { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
-  {
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-      require("monokai-pro").setup {
-        transparent_background = true,
-        filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
-      }
-    end,
-  },
+  { import = "astrocommunity.color.tint-nvim" },
 
+  -- Plugins
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.completion.codeium-vim" },
 
-  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  -- Motions
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.leap-nvim" },
 
+  -- Packs
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.typescript" },
@@ -25,11 +25,4 @@ return {
   { import = "astrocommunity.pack.dart" },
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.tailwindcss" },
-
-  { import = "astrocommunity.motion.flit-nvim" },
-  { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.motion.leap-nvim" },
-
-  { import = "astrocommunity.color.tint-nvim" },
 }
