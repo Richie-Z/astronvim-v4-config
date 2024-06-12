a98350fde07e9b114d6d36b94ed1b51b85cce903
Richie-Z <zakariarichie@yahoo.com>
2024-06-12 09:00:46 +0700

    feat: add marks motion

--- lua/community.lua
+++ lua/community.lua
@@ -24,6 +24,7 @@ return {
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.marks-nvim" },
 
   -- Packs
   { import = "astrocommunity.pack.bash" },
