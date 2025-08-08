42cfcb938e53d27a7d933af483b498f51b72a2ee
Richie-Z <zakariarichie@yahoo.com>
2025-08-09 03:37:03 +0700

    feat: add cfml

---
+++ lua/plugins/cfml.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "wsdjeg/vim-cfml",
+  -- {
+  --   "hannaeckert/cfmlsp.nvim",
+  --   dependencies = {
+  --     "neovim/nvim-lspconfig",
+  --     "hrsh7th/cmp-nvim-lsp",
+  --   },
+  --   opts = {},
+  -- },
+}
